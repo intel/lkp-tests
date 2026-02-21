ba60e3bcee9578ecb1f7956fd04fa1fe4b37c3aa
Philip Li <philip.li@intel.com>
2026-02-21 22:34:27 +0800

    spec/stress_ng_spec.rb: ignore stress-ng-1ssd.yaml
    [2026-02-21T13:46:58.348Z] 2) stress-ng has sorted test names in /zdci/src/lkp/jobs/stress-ng-1ssd.yaml
    [2026-02-21T13:46:58.348Z] Failure/Error:
    [2026-02-21T13:46:58.348Z] load_values_from_job_yamls(File.basename(job_yaml, '.yaml'), include_comment: false) do |document|
    [2026-02-21T13:46:58.348Z] expect(document['stress-ng']['test']).to be_sorted
    [2026-02-21T13:46:58.348Z] end
    [2026-02-21T13:46:58.348Z]
    [2026-02-21T13:46:58.348Z] Psych::SyntaxError:
    [2026-02-21T13:46:58.348Z] (<unknown>): mapping values are not allowed in this context at line 1 column 30
    
    Signed-off-by: Philip Li <philip.li@intel.com>

--- spec/stress_ng_spec.rb
+++ spec/stress_ng_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe 'stress-ng' do
   Dir[LKP::Path.src('jobs', 'stress-ng*.yaml')].each do |job_yaml|
+    next if job_yaml =~ /stress-ng-1ssd.yaml/
+
     it "has sorted test names in #{job_yaml}" do
       load_values_from_job_yamls(File.basename(job_yaml, '.yaml'), include_comment: false) do |document|
         expect(document['stress-ng']['test']).to be_sorted
