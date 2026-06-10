intel/lkp-tests

commit c87c0b86a6b78af31d36ba95c9a00c135bca56a8

programs: refactor PKGBUILD to remove duplicated code
Signed-off-by: Philip Li <philip.li@intel.com>

programs/autonuma-benchmark/pkg/PKGBUILD
@@ -9,12 +9,3 @@ sha256sums=('SKIP')
 
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
-prepare()
-{
-	prepare_benchmark_path
-}
-
-package()
-{
-	pack_src_pkg_contents
-}

programs/bpftrace/pkg/PKGBUILD
@@ -11,11 +11,6 @@ md5sums=('SKIP')
 
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
-prepare()
-{
-	prepare_benchmark_path
-}
-
 package()
 {
     mkdir -p $benchmark_path

programs/cassandra/pkg/PKGBUILD
@@ -18,7 +18,3 @@ prepare()
     rename_versioned_src_pkg_dir apache-$pkgname-$pkgver
 }
 
-package()
-{
-	pack_src_pkg_contents
-}

programs/chromeswap/pkg/PKGBUILD
@@ -9,12 +9,3 @@ md5sums=('SKIP')
 
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
-prepare()
-{
-	prepare_benchmark_path
-}
-
-package()
-{
-	pack_src_pkg_contents
-}

programs/kernbench/pkg/PKGBUILD
@@ -11,11 +11,6 @@ md5sums=('SKIP' 'SKIP')
 
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
-prepare()
-{
-	prepare_benchmark_path
-}
-
 package()
 {
 	pack_contents "$srcdir/linux-${kernel_ver}/" "$benchmark_path/linux"

programs/ku-latency/pkg/PKGBUILD
@@ -12,11 +12,6 @@ md5sums=('SKIP' 'SKIP' 'SKIP')
 . $LKP_SRC/lib/install.sh
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
-prepare()
-{
-	prepare_benchmark_path
-}
-
 patch_source()
 {
 	# PWD=/tmp/lkp/ku-latency/src, srcdir=/tmp/lkp/ku-latency/src

programs/leaking-addresses/pkg/PKGBUILD
@@ -9,11 +9,6 @@ md5sums=('SKIP')
 
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
-prepare()
-{
-	prepare_benchmark_path
-}
-
 pkgver()
 {
 	# get the last commit of leaking_addresses.pl

programs/nuttcp/pkg/PKGBUILD
@@ -10,11 +10,6 @@ md5sums=('SKIP')
 
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
-prepare()
-{
-	prepare_benchmark_path
-}
-
 package()
 {
 	cd "${srcdir}"

commit 81f88e13dc66b5501016ad47cb7f2c118ba4122a

programs: add no-op build() to non-compiling PKGBUILDs
Commit c87c0b86a6b7 ("programs: refactor PKGBUILD to remove duplicated
code") added default prepare(), build() and package() functions to
lib/tests/pkgbuild.sh and dropped the now-duplicated definitions from the
individual PKGBUILDs.

The default build() runs make_src_pkg, which does:
    cd_src_pkg_dir   # cd $srcdir/$pkgname
    ...
    make -j$(nproc)

This is correct only for packages whose source is a buildable directory
named after $pkgname. Several packages, however, never defined build() at
all and have nothing to compile (prebuilt binaries, plain scripts,
git/tarball sources that are packed as-is, or sources compiled directly in
package()). Before the refactor makepkg simply skipped the build step for
them; afterwards they wrongly inherit the default build() and fail

Restore the original "no build" behaviour by overriding the inherited
default with a no-op build() in the affected packages:
  kernbench, autonuma-benchmark, bpftrace, cassandra, chromeswap,
  ku-latency, leaking-addresses, libhugetlbfs-test, nuttcp, pybench, stream

Fixes: c87c0b86a6b7 ("programs: refactor PKGBUILD to remove duplicated code")

Signed-off-by: Suneeth D <Suneeth.D@amd.com>

programs/autonuma-benchmark/pkg/PKGBUILD
@@ -9,3 +9,9 @@ sha256sums=('SKIP')
 
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
+# No compile step; override the default build() (make_src_pkg from pkgbuild.sh)
+# so install doesn't run make on the unbuilt source.
+build()
+{
+	:
+}

programs/bpftrace/pkg/PKGBUILD
@@ -11,6 +11,13 @@ md5sums=('SKIP')
 
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
+# No compile step; override the default build() (make_src_pkg from pkgbuild.sh)
+# so install doesn't run make on the unbuilt source.
+build()
+{
+	:
+}
+
 package()
 {
     mkdir -p $benchmark_path

programs/cassandra/pkg/PKGBUILD
@@ -12,6 +12,13 @@ md5sums=('SKIP')
 . $LKP_SRC/lib/reproduce-log.sh
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
+# No compile step; override the default build() (make_src_pkg from pkgbuild.sh)
+# so install doesn't run make on the unbuilt source.
+build()
+{
+	:
+}
+
 prepare()
 {
 	prepare_benchmark_path

programs/chromeswap/pkg/PKGBUILD
@@ -9,3 +9,9 @@ md5sums=('SKIP')
 
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
+# No compile step; override the default build() (make_src_pkg from pkgbuild.sh)
+# so install doesn't run make on the unbuilt source.
+build()
+{
+	:
+}

programs/kernbench/pkg/PKGBUILD
@@ -11,6 +11,14 @@ md5sums=('SKIP' 'SKIP')
 
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
+# kernbench has nothing to compile (the kernbench source is a single script and
+# the linux tarball is only packed), so override the default build() which would
+# otherwise run make_src_pkg and try to 'cd' into the non-directory src/kernbench.
+build()
+{
+	:
+}
+
 package()
 {
 	pack_contents "$srcdir/linux-${kernel_ver}/" "$benchmark_path/linux"

programs/ku-latency/pkg/PKGBUILD
@@ -12,6 +12,13 @@ md5sums=('SKIP' 'SKIP' 'SKIP')
 . $LKP_SRC/lib/install.sh
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
+# Compilation happens in package() via gcc; override the default build()
+# (make_src_pkg from pkgbuild.sh) so install doesn't run make on the unbuilt source.
+build()
+{
+	:
+}
+
 patch_source()
 {
 	# PWD=/tmp/lkp/ku-latency/src, srcdir=/tmp/lkp/ku-latency/src

programs/leaking-addresses/pkg/PKGBUILD
@@ -9,6 +9,13 @@ md5sums=('SKIP')
 
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
+# No compile step; override the default build() (make_src_pkg from pkgbuild.sh)
+# so install doesn't run make on the unbuilt source.
+build()
+{
+	:
+}
+
 pkgver()
 {
 	# get the last commit of leaking_addresses.pl

programs/nuttcp/pkg/PKGBUILD
@@ -10,6 +10,13 @@ md5sums=('SKIP')
 
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
+# Prebuilt binary, no compile step; override the default build() (make_src_pkg
+# from pkgbuild.sh) so install doesn't run make on the unbuilt source.
+build()
+{
+	:
+}
+
 package()
 {
 	cd "${srcdir}"

programs/pybench/pkg/PKGBUILD
@@ -9,6 +9,13 @@ md5sums=('e3ca0e887e4310896d32dd1cbd258a94')
 
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
+# No compile step; override the default build() (make_src_pkg from pkgbuild.sh)
+# so install doesn't run make on the unbuilt source.
+build()
+{
+	:
+}
+
 prepare()
 {
 	prepare_benchmark_path

programs/stream/pkg/PKGBUILD
@@ -9,6 +9,13 @@ md5sums=('SKIP')
 
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
+# No compile step; override the default build() (make_src_pkg from pkgbuild.sh)
+# so install doesn't run make on the unbuilt source.
+build()
+{
+	:
+}
+
 package()
 {
 	pack_src_pkg_contents stream.c

programs/libhugetlbfs-test/pkg/PKGBUILD
@@ -12,6 +12,13 @@ md5sums=('SKIP' 'SKIP')
 . $LKP_SRC/lib/install.sh
 . $LKP_SRC/lib/tests/pkgbuild.sh
 
+# No compile step; override the default build() (make_src_pkg from pkgbuild.sh)
+# so install doesn't run make on the unbuilt source.
+build()
+{
+	:
+}
+
 patch_source()
 {
 	[ "$DISTRO" = "centos" ] || return 0